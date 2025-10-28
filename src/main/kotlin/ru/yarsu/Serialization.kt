100ed7c5c5761cd1d50b213ddcfa5f8f67c9a80c
Rumpel0726 <i_kalinin_04@mail.ru>
2025-10-28 21:22:45 +0300

    The project structure has been updated and Pebble and Postgres plugins have been added (#57211011)

--- src/main/kotlin/ru/yarsu/Serialization.kt
+++ src/main/kotlin/ru/yarsu/Serialization.kt
@@ -4,10 +4,10 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Application.configureRouting() {
+fun Application.configureSerialization() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        get("/json/kotlinx-serialization") {
+            call.respond(mapOf("hello" to "world"))
         }
     }
 }
